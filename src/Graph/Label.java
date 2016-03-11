d8cf0b0b1ce086bcec29ca14fbe65d50d2054300
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-03-10 22:07:05 -0300

    * Added enumeration to represent graph node labes

---
+++ src/Graph/Label.java
@@ -0,0 +1,10 @@
+package Graph;
+
+/**
+ * That enumeration describes the labels used on the graph nodes.
+ *
+ * @author Reuel
+ */
+public enum Label {
+    AXIOM, CONSTANT, HYPOTHESIS, THEOREM, VARIABLE
+}
