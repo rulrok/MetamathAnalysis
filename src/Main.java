c7c0a51ffddac706c8272295635914566593bf7e
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-03-10 21:42:40 -0300

    * Ignoring .log files * Added file example of creating a neo4j connection

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,47 @@
+
+import java.io.File;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 
 /**
  *
  * @author Reuel
  */
 public class Main {
- 
+
     public static void main(String[] args) {
-        String DB_PATH = "";
-        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
-        
-        
+        String DB_PATH = "db/metamath";
+        File dbPath = new File(DB_PATH);
+        GraphDatabaseService graphDb;
+        graphDb = new GraphDatabaseFactory()
+                .newEmbeddedDatabaseBuilder(dbPath)
+                .setConfig(GraphDatabaseSettings.pagecache_memory, "512M")
+                .setConfig(GraphDatabaseSettings.string_block_size, "60")
+                .setConfig(GraphDatabaseSettings.array_block_size, "300")
+                .newGraphDatabase();
+
+        registerShutdownHook(graphDb);
+
+        //
+        try (Transaction tx = graphDb.beginTx()) {
+            // Database operations go here
+            tx.success();
+        }
+
+        graphDb.shutdown();
+    }
+
+    private static void registerShutdownHook(final GraphDatabaseService graphDb) {
+        // Registers a shutdown hook for the Neo4j instance so that it
+        // shuts down nicely when the VM exits (even if you "Ctrl-C" the
+        // running application).
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                graphDb.shutdown();
+            }
+        });
     }
 }
