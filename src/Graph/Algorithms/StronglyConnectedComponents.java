d062926d943f3b303853ec353b8173468b66cbff
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-04-13 13:32:08 -0300

    * Finishing implementing the pseudo-code but it still refuses to work apparently :(

--- src/Graph/Algorithms/StronglyConnectedComponents.java
+++ src/Graph/Algorithms/StronglyConnectedComponents.java
@@ -2,19 +2,16 @@ package Graph.Algorithms;
 
 import Graph.*;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.PathExpanderBuilder;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.ResourceIterator;
-import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.traversal.*;
 
@@ -29,10 +26,12 @@ public class StronglyConnectedComponents {
     /**
      * HelperNode is an antificial node linking to everyone else in the graph.
      */
-    private Node helperNode;
+    private Node x;
     private TraversalDescription dfsTD;
     private int N;
     private List<Node> L;
+    private List<List<Node>> components;
+    private List<Long> T;
     private Map<Long, Integer> low;
     private Map<Long, Integer> dfsnum;
 
@@ -48,9 +47,10 @@ public class StronglyConnectedComponents {
                 .depthFirst()
                 .relationships(RelTypes.SUPPORTS, Direction.BOTH)
                 .relationships(HelperRel.HELPER, Direction.OUTGOING);
-        
+
         low = new TreeMap<>();
         dfsnum = new TreeMap<>();
+        components = new LinkedList<>();
     }
 
     /**
@@ -65,11 +65,11 @@ public class StronglyConnectedComponents {
         try (Transaction tx = graph.beginTx()) {
 
             /* make a new vertex x with edges x->v for all v */
-            helperNode = graph.createNode();
+            x = graph.createNode();
             ResourceIterator<Node> axiomNodes = graph.findNodes(Label.AXIOM);
             for (; axiomNodes.hasNext();) {
                 Node axiom = axiomNodes.next();
-                helperNode.createRelationshipTo(axiom, HelperRel.HELPER);
+                x.createRelationshipTo(axiom, HelperRel.HELPER);
             }
 
             /* initialize a counter N to zero */
@@ -77,34 +77,36 @@ public class StronglyConnectedComponents {
 
             /*  initialize list L to empty */
             L = new ArrayList<>();
-            
+
             /* build directed tree T, initially a single vertex {x} */
-            
-            
+            T = new LinkedList<>();
+            T.add(x.getId());
+
             /* visit(x) */
-            for (Path position : dfsTD.traverse(helperNode)) {
-                visit(position.endNode());
-            }
+//            for (Path position : dfsTD.traverse(helperNode)) {
+//                visit(position.endNode());
+//            }
+            visit(x);
 
             tx.failure();
         }
 
     }
-    
-    private void visit (Node p){
-        
+
+    private void visit(Node p) {
+
         /* add p to L */
         L.add(p);
-        
+
         /* dfsnum(p) = N */
-        dfsnum.put(p.getId(), N );
-        
+        dfsnum.put(p.getId(), N);
+
         /* increment N */
         N++;
-        
+
         /* low(p) = dfsnum(p) */
         low.put(p.getId(), dfsnum.get(p.getId()));
-        
+
         /*  for each edge p->q
                 if q is not already in T
                 {
@@ -113,18 +115,38 @@ public class StronglyConnectedComponents {
                     low(p) = min(low(p), low(q))
                 } else 
                     low(p) = min(low(p), dfsnum(q)) 
-        */
-        for (Relationship relationship : p.getRelationships()) {
-        } 
-        
+         */
+        for (Relationship p_q : p.getRelationships(HelperRel.HELPER, RelTypes.SUPPORTS)) {
+            
+            Node q = p_q.getEndNode();
+            if (!T.contains(q.getId())) {
+                T.add(q.getId());
+                visit(q);
+                low.put(p.getId(), Math.min(low.get(p.getId()), low.get(q.getId())));
+            } else {
+                low.put(p.getId(), Math.min(low.get(p.getId()), dfsnum.get(q.getId())));
+            }
+
+        }
+
         /*     if low(p)=dfsnum(p)
         {
         output "component:"
         repeat
-        remove last element v from L
-        output v
-        remove v from G
+            remove last element v from L
+            output v
+            remove v from G
         until v=p
         } */
+        List<Node> currentComponent = new LinkedList<>();
+        if ((int) low.get(p.getId()) == (int) dfsnum.get(p.getId())) {
+            Node v;
+            do {
+                v = L.remove(L.size() - 1);
+                currentComponent.add(v);
+                v.delete();
+            } while (v.getId() != p.getId());
+            components.add(currentComponent);
+        }
     }
 }
