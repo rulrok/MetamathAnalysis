a84dd6beedfd84fd2248615fb33fd18325eca685
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-04-21 11:34:49 -0300

    * Simple implementation to calculate distributions. It runs a n.log(n) because we are using a TreeMap. It needs improvements yet.

--- src/Graph/Algorithms/DegreeDistribution.java
+++ src/Graph/Algorithms/DegreeDistribution.java
@@ -1,8 +1,14 @@
 package Graph.Algorithms;
 
 import java.util.Collections;
+import java.util.Dictionary;
 import java.util.Map;
+import java.util.TreeMap;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.ResourceIterable;
+import org.neo4j.tooling.GlobalGraphOperations;
 
 /**
  *
@@ -10,26 +16,49 @@ import org.neo4j.graphdb.GraphDatabaseService;
  */
 public class DegreeDistribution {
 
-    private Map<Integer, Double> outterDegrees;
-    private Map<Integer, Double> innerDegrees;
-    private Map<Integer, Double> allDegrees;
-
+    private final Map<Integer, Integer> outterDegrees;
+    private final Map<Integer, Integer> innerDegrees;
+    private final Map<Integer, Integer> allDegrees;
     private final GraphDatabaseService graph;
 
     public DegreeDistribution(GraphDatabaseService graph) {
         this.graph = graph;
+
+        outterDegrees = new TreeMap<>();
+        innerDegrees = new TreeMap<>();
+        allDegrees = new TreeMap<>();
     }
 
-    public Map<Integer, Double> getOutterDegrees() {
+    public void calculate() {
+
+        outterDegrees.clear();
+        innerDegrees.clear();
+        allDegrees.clear();
+
+        ResourceIterable<Node> allNodes = GlobalGraphOperations.at(graph).getAllNodes();
+
+        allNodes.forEach((Node node) -> {
+            int allDegree = node.getDegree();
+            allDegrees.put(allDegree, allDegrees.getOrDefault(allDegree, 0) + 1);
+
+            int innerDegree = node.getDegree(Direction.INCOMING);
+            innerDegrees.put(innerDegree, innerDegrees.getOrDefault(innerDegree, 0) + 1);
+
+            int outterDegree = node.getDegree(Direction.OUTGOING);
+            outterDegrees.put(outterDegree, outterDegrees.getOrDefault(outterDegree, 0) + 1);
+        });
+    }
+
+    public Map<Integer, Integer> getOutterDegrees() {
 
         return Collections.unmodifiableMap(outterDegrees);
     }
 
-    public Map<Integer, Double> getInnerDegrees() {
+    public Map<Integer, Integer> getInnerDegrees() {
         return Collections.unmodifiableMap(innerDegrees);
     }
 
-    public Map<Integer, Double> getAllDegrees() {
+    public Map<Integer, Integer> getAllDegrees() {
         return Collections.unmodifiableMap(allDegrees);
     }
 
