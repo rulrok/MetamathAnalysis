cd0d78fb579e74ab6cfe3f9723c520a3f58bd412
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-04-21 11:08:42 -0300

    * Add unmodifiable getters for each map

--- src/Graph/Algorithms/DegreeDistribution.java
+++ src/Graph/Algorithms/DegreeDistribution.java
@@ -1,5 +1,6 @@
 package Graph.Algorithms;
 
+import java.util.Collections;
 import java.util.Map;
 import org.neo4j.graphdb.GraphDatabaseService;
 
@@ -12,12 +13,24 @@ public class DegreeDistribution {
     private Map<Integer, Double> outterDegrees;
     private Map<Integer, Double> innerDegrees;
     private Map<Integer, Double> allDegrees;
-    
+
     private final GraphDatabaseService graph;
 
     public DegreeDistribution(GraphDatabaseService graph) {
         this.graph = graph;
     }
-    
-    
+
+    public Map<Integer, Double> getOutterDegrees() {
+
+        return Collections.unmodifiableMap(outterDegrees);
+    }
+
+    public Map<Integer, Double> getInnerDegrees() {
+        return Collections.unmodifiableMap(innerDegrees);
+    }
+
+    public Map<Integer, Double> getAllDegrees() {
+        return Collections.unmodifiableMap(allDegrees);
+    }
+
 }
