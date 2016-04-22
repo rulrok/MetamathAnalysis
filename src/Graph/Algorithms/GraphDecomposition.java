3868ff50a41d1d12114d2ac8ea4f969e8bc5a835
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-04-22 17:36:47 -0300

    * Generalize the algorithm of decomposition

--- src/Graph/Algorithms/GraphDecomposition.java
+++ src/Graph/Algorithms/GraphDecomposition.java
@@ -30,51 +30,58 @@ public class GraphDecomposition {
         components = new ArrayList<>();
     }
 
-    public List<List<Node>> execute(DecompositionTarget direction, List<Node> initialNodes) {
+    public List<List<Node>> execute(DecompositionTarget decompositionTarget, List<Node> initialNodes) {
 
         if (initialNodes.isEmpty() || initialNodes.contains(null)) {
             return components;
         }
 
+        //Create a copy of the passed list so we don't change the outsite list
         LinkedList<Node> initialNodesList = new LinkedList<>(initialNodes);
 
-        TraversalDescription traversalDescription = graph.traversalDescription();
+        TraversalDescription bfs = graph.traversalDescription().breadthFirst();
+        Direction direction = Direction.BOTH;
 
-        if (direction == DecompositionTarget.SINK) {
-            TraversalDescription bfs = traversalDescription
-                    .breadthFirst()
+        if (decompositionTarget == DecompositionTarget.SINK) {
+            bfs = bfs
                     .order(BranchOrderingPolicies.PREORDER_BREADTH_FIRST)
                     .evaluator(new SinkEvaluator());
+            direction = Direction.INCOMING;
 
-            try (Transaction tx = graph.beginTx()) {
+        }
 
-                do {
-                    List<Node> component = new LinkedList<>();
-                    bfs.traverse(initialNodesList).forEach((Path path) -> {
-                        Node endNode = path.endNode();
-                        endNode.getRelationships(Direction.INCOMING).forEach(relationship -> {
-                            relationship.delete();
-                        });
-                        component.add(endNode);
-                        initialNodesList.removeIf((Node node) -> {
-                            return endNode.equals(node);
-                        });
-                        endNode.delete();
+        if (decompositionTarget == DecompositionTarget.SOURCE) {
+            bfs = bfs
+                    .order(BranchOrderingPolicies.PREORDER_BREADTH_FIRST);
+            direction = Direction.OUTGOING;
+        }
 
+        try (Transaction tx = graph.beginTx()) {
+
+            do {
+                List<Node> component = new LinkedList<>();
+                final Direction relationshipDirection = direction;
+                bfs.traverse(initialNodesList).forEach((Path path) -> {
+                    Node endNode = path.endNode();
+                    endNode.getRelationships(relationshipDirection).forEach(relationship -> {
+                        relationship.delete();
                     });
-                    if (!component.isEmpty()) {
-                        components.add(component);
-                    } else {
-                        component.addAll(initialNodes);
-                        components.add(component);
-                        break;
-                    }
-                } while (true);
-                tx.failure();
-            }
+                    component.add(endNode);
+                    initialNodesList.removeIf((Node node) -> {
+                        return endNode.equals(node);
+                    });
+                    endNode.delete();
 
-        } else if (direction == DecompositionTarget.SOURCE) {
-
+                });
+                if (!component.isEmpty()) {
+                    components.add(component);
+                } else {
+                    component.addAll(initialNodes);
+                    components.add(component);
+                    break;
+                }
+            } while (true);
+            tx.failure();
         }
 
         return components;
