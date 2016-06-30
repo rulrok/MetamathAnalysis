d0fe385addab9fea711f3d963d51c39b22404b2a
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-06-29 22:08:14 -0300

    * Create new graph, yet the nodes are being duplicated

--- src/Graph/Algorithms/SmallerInducedSubgraph.java
+++ src/Graph/Algorithms/SmallerInducedSubgraph.java
@@ -2,6 +2,7 @@ package Graph.Algorithms;
 
 import Graph.GraphFactory;
 import java.util.ArrayList;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
@@ -16,15 +17,18 @@ import org.neo4j.tooling.GlobalGraphOperations;
 public class SmallerInducedSubgraph {
 
     private final GraphDatabaseService graph;
+    private final GraphDatabaseService outputGraph;
 
-    public SmallerInducedSubgraph(GraphDatabaseService graph) {
+    public SmallerInducedSubgraph(GraphDatabaseService graph, GraphDatabaseService outputGraph) {
         this.graph = graph;
+        this.outputGraph = outputGraph;
     }
 
     public void execute(String... nodeNames) {
         try (Transaction tx = graph.beginTx()) {
             ArrayList<Node> nodes = new ArrayList<>(nodeNames.length);
 
+            //First, get the node proxies based on the input string names
             for (String nodeName : nodeNames) {
                 for (Label label : GlobalGraphOperations.at(graph).getAllLabels()) {
                     Node foundNode = graph.findNode(label, "name", nodeName);
@@ -35,13 +39,63 @@ public class SmallerInducedSubgraph {
                 }
             }
 
-            System.out.println("");
+            //Second, construct the graph based on the nodes and relationships
+            try (Transaction txOutputGraph = outputGraph.beginTx()) {
+
+                for (Node node : nodes) {
+                    Node newNode = outputGraph.createNode();
+                    newNode.setProperty("name", node.getProperty("name"));
+
+                    //Verify the incoming relationships from other nodes
+                    for (Relationship inRels : node.getRelationships(Direction.INCOMING)) {
+                        //'node' is the end node of the relationship
+                        //'startNode'---->'node'
+                        Node startNode = inRels.getStartNode();
+
+                        //'startNode' is not one of the desired nodes, move on...
+                        if (!nodes.contains(startNode)) {
+                            continue;
+                        }
+
+                        Node newStartNode = outputGraph.createNode();
+                        newStartNode.setProperty("name", startNode.getProperty("name"));
+                        newStartNode.createRelationshipTo(newNode, inRels.getType());
+
+                    }
+
+                    for (Relationship outRels : node.getRelationships(Direction.OUTGOING)) {
+                        //'node' is the start node of the relationship
+                        //'node'---->'endNode'
+                        Node endNode = outRels.getEndNode();
+
+                        //'endNode' is not one of the desired nodes, move on...
+                        if (!nodes.contains(endNode)) {
+                            continue;
+                        }
+
+                        Node newEndNode = outputGraph.createNode();
+                        newEndNode.setProperty("name", endNode.getProperty("name"));
+                        newNode.createRelationshipTo(newEndNode, outRels.getType());
+                    }
+                }
+
+                txOutputGraph.success(); //Persist the new graph
+            }
+
+            tx.failure(); //Make sure we do not modify the original graph
         }
     }
 
     public static void main(String[] args) {
+
+        //Prepare databases
         GraphDatabaseService graph = GraphFactory.makeDefaultMetamathGraph();
-        SmallerInducedSubgraph msgi = new SmallerInducedSubgraph(graph);
+        GraphDatabaseService outputGraph = GraphFactory.makeGraph("db/Betweenness");
+
+        //Prepare algorithm object
+        SmallerInducedSubgraph msgi = new SmallerInducedSubgraph(graph, outputGraph);
+
+        //Execute it
         msgi.execute(
                 "fmpt",
                 "dvhlvec",
