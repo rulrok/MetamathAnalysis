8e4e4f5b468d5cc5bcd55dee11a93a1095df01ba
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-07-01 21:08:53 -0300

    * Add method to easily add a supersink and supersource to a graph

---
+++ src/Graph/Algorithms/SuperSinkSuperSource.java
@@ -0,0 +1,66 @@
+package Graph.Algorithms;
+
+import Graph.Algorithms.Decomposition.Evaluators.SinkEvaluator;
+import Graph.Algorithms.Decomposition.Evaluators.SourceEvaluator;
+import Graph.GraphFactory;
+import Graph.Label;
+import Graph.RelTypes;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.traversal.BranchOrderingPolicies;
+import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Uniqueness;
+
+/**
+ * Adds a super sink (S) and super source (T) to a graph.
+ * It changes the graph permanently.
+ * 
+ * @author Reuel
+ */
+public class SuperSinkSuperSource {
+
+    private final GraphDatabaseService graph;
+
+    public SuperSinkSuperSource(GraphDatabaseService graph) {
+        this.graph = graph;
+    }
+
+    public void execute() {
+        try (Transaction tx = graph.beginTx()) {
+
+            //Create artificial supersink and supersource nodes
+            TraversalDescription sourceTraverser = graph.traversalDescription()
+                    .breadthFirst()
+                    .order(BranchOrderingPolicies.PREORDER_DEPTH_FIRST)
+                    .uniqueness(Uniqueness.NODE_GLOBAL)
+                    .evaluator(new SourceEvaluator());
+
+            Node s = graph.createNode(Label.UNKNOWN);
+            s.setProperty("name", "S");
+            sourceTraverser.traverse(graph.getNodeById(0)).nodes().forEach(source -> {
+                s.createRelationshipTo(source, RelTypes.UNKNOWN);
+            });
+
+            Node t = graph.createNode(Label.UNKNOWN);
+            t.setProperty("name", "T");
+            TraversalDescription sinkTraverser = graph.traversalDescription()
+                    .breadthFirst()
+                    .order(BranchOrderingPolicies.PREORDER_DEPTH_FIRST)
+                    .uniqueness(Uniqueness.NODE_GLOBAL)
+                    .evaluator(new SinkEvaluator());
+            sinkTraverser.traverse(graph.getNodeById(0)).nodes().forEach(sink -> {
+                sink.createRelationshipTo(t, RelTypes.UNKNOWN);
+            });
+
+            tx.success();
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphDatabaseService graph = GraphFactory.copyGraph("db/metamath", "db/super_metamath");
+
+        SuperSinkSuperSource ssss = new SuperSinkSuperSource(graph);
+        ssss.execute();
+    }
+}
