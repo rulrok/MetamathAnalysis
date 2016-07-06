a116bb6c55be6d40c5a4f16d0480a00dfd6857c5
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-07-06 00:11:46 -0300

    * Update label evaluator to allow multiple labels and say if they must be exclusive or not to evaluate a path

--- src/Graph/Algorithms/Decomposition/Evaluators/LabelEvaluator.java
+++ src/Graph/Algorithms/Decomposition/Evaluators/LabelEvaluator.java
@@ -1,6 +1,9 @@
 package Graph.Algorithms.Decomposition.Evaluators;
 
 import Graph.Label;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.traversal.Evaluation;
@@ -12,16 +15,37 @@ import org.neo4j.graphdb.traversal.Evaluator;
  */
 public class LabelEvaluator implements Evaluator {
 
-    private final Label label;
+    private final List<Label> labels = new ArrayList<>();
+    private final boolean exclusive;
 
     public LabelEvaluator(Label label) {
-        this.label = label;
+        labels.add(label);
+        exclusive = true;
+    }
+
+    public LabelEvaluator(boolean exclusive, Label... labels) {
+        this.labels.addAll(Arrays.asList(labels));
+        this.exclusive = exclusive;
     }
 
     @Override
     public Evaluation evaluate(Path path) {
         Node lastNode = path.endNode();
-        if (lastNode.hasLabel(label)) {
+        boolean include;
+        
+        if (exclusive) {
+            include = true;
+            for (Label l : labels) {
+                include &= lastNode.hasLabel(l);
+            }
+        } else {
+            include = false;
+            for (Label l : labels) {
+                include |= lastNode.hasLabel(l);
+            }
+        }
+
+        if (include) {
             return Evaluation.INCLUDE_AND_CONTINUE;
         }
 
