5280f75be24f80e4a20150113bafd0c1c1bb4065
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-08-19 09:11:44 -0300

    * Move method to its own class

---
+++ src/Graph/Algorithms/Export/UniqueSequenceGenerator.java
@@ -0,0 +1,36 @@
+package Graph.Algorithms.Export;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Reuel
+ */
+public class UniqueSequenceGenerator {
+
+    private long guid;
+    private final Map<Long, Long> inputs;
+
+    public UniqueSequenceGenerator() {
+        this.guid = 0;
+        this.inputs = new HashMap<>();
+    }
+
+    /**
+     * For each input value, it returns a unique sequencial value. If the same
+     * number is input again, its old associated value is returned.
+     *
+     * @param input
+     * @return
+     */
+    public long uniqueSequencialId(long input) {
+
+        if (inputs.containsKey(input)) {
+            return inputs.get(input);
+        } else {
+            inputs.put(input, ++guid);
+            return guid;
+        }
+    }
+}
