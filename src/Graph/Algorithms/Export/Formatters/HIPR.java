295407e142072a94bbe24014b13f40b17ebe12fe
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-08-22 10:44:40 -0300

    * Fix HIPR formatter

--- src/Graph/Algorithms/Export/Formatters/HIPR.java
+++ src/Graph/Algorithms/Export/Formatters/HIPR.java
@@ -2,7 +2,7 @@ package Graph.Algorithms.Export.Formatters;
 
 import Graph.Algorithms.Export.UniqueSequenceGenerator;
 import Graph.Label;
-import java.util.Set;
+import java.util.List;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
@@ -22,21 +22,19 @@ public class HIPR implements IGraphFormatter {
     }
 
     @Override
-    public CharSequence format(GraphDatabaseService graph, Set<Node> nodesToPrint, Set<Relationship> relsToPrint) {
+    public CharSequence format(GraphDatabaseService graph, List<Node> nodesToPrint, List<Relationship> relsToPrint) {
         StringBuilder output = new StringBuilder();
 
         try (final Transaction tx = graph.beginTx()) {
 
-            Node S = graph.findNode(Label.AXIOM, "name", "S");
-            Node T = graph.findNode(Label.THEOREM, "name", "T");
-
             //Headers
             output.append("p max ").append(nodesToPrint.size()).append(' ').append(relsToPrint.size()).append(System.lineSeparator());
-            output.append("n ").append(usg.uniqueSequencialId(S.getId())).append(" s").append(System.lineSeparator());
-            output.append("n ").append(usg.uniqueSequencialId(T.getId())).append(" t").append(System.lineSeparator());
+            output.append("n ").append("<s_id>").append(" s").append(System.lineSeparator());
+            output.append("n ").append("<t_id>").append(" t").append(System.lineSeparator());
 
             //Arcs
-            relsToPrint.forEach(rel -> {
+            for (Relationship rel : relsToPrint) {
+
                 Node startNode = rel.getStartNode();
                 Node endNode = rel.getEndNode();
 
@@ -50,7 +48,19 @@ public class HIPR implements IGraphFormatter {
                         //The below line is an extra only to make it easier to interpret the output
                         .append("( ").append(startNodeName).append(" -> ").append(endNodeName).append(" )")
                         .append(System.lineSeparator());
-            });
+            }
+
+            Node S = graph.findNode(Label.AXIOM, "name", "S");
+            Node T = graph.findNode(Label.THEOREM, "name", "T");
+            long S_id = usg.uniqueSequencialId(S.getId());
+            long T_id = usg.uniqueSequencialId(T.getId());
+
+            int s_index = output.indexOf("<s_id>");
+            output.replace(s_index, s_index + Long.toString((S_id)).length() + 1, Long.toString(S_id));
+
+            int t_index = output.indexOf("<t_id>");
+            output.replace(t_index, t_index + Long.toString((T_id)).length() + 1, Long.toString(T_id));
+
             tx.failure();
         }
         return output;
