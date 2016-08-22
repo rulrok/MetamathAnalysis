fd056d69b0dca94805792193887931033dab381a
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-08-22 12:18:36 -0300

    * Add custom weight to HIPR formatter

--- src/Graph/Algorithms/Export/Formatters/HiprFormatter.java
+++ src/Graph/Algorithms/Export/Formatters/HiprFormatter.java
@@ -1,8 +1,10 @@
 package Graph.Algorithms.Export.Formatters;
 
 import Graph.Algorithms.Export.UniqueSequenceGenerator;
+import Graph.Algorithms.GraphToHIPRtxt;
 import Graph.Label;
 import java.util.List;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
@@ -17,8 +19,30 @@ public class HiprFormatter implements IGraphFormatter {
 
     UniqueSequenceGenerator usg;
 
-    public HiprFormatter() {
+    private final String SourceNodeName;
+    private final String SinkNodeName;
+
+    private boolean superSinkSourceCustomWeight;
+
+    public HiprFormatter(String sourceNodeName, String sinkNodeName) {
+        SourceNodeName = sourceNodeName;
+        SinkNodeName = sinkNodeName;
+
         usg = new UniqueSequenceGenerator();
+    }
+
+    /**
+     * The arcs coming out of a SuperSource will have the weight set as the
+     * number of the outter degree of the nodes that they each reach. The same
+     * occurs for the SuperSink, but the weight of the arcs are being defined
+     * based on the inner degree of the nodes that reach the SuperSink.
+     *
+     * @return
+     */
+    public HiprFormatter withCustomWeightForSuperSinkAndSource() {
+        superSinkSourceCustomWeight = true;
+
+        return this;
     }
 
     @Override
@@ -33,6 +57,7 @@ public class HiprFormatter implements IGraphFormatter {
             output.append("n ").append("<t_id>").append(" t").append(System.lineSeparator());
 
             //Arcs
+            int weight;
             for (Relationship rel : relsToPrint) {
 
                 Node startNode = rel.getStartNode();
@@ -44,14 +69,27 @@ public class HiprFormatter implements IGraphFormatter {
                 Object startNodeName = startNode.getProperty("name");
                 Object endNodeName = endNode.getProperty("name");
 
-                output.append("a ").append(startNodeId).append(' ').append(endNodeId).append(" 1 ")
-                        //The below line is an extra only to make it easier to interpret the output
-                        .append("( ").append(startNodeName).append(" -> ").append(endNodeName).append(" )")
+                weight = 1;
+                if (superSinkSourceCustomWeight) {
+                    if (startNodeName.equals("S")) {
+                        //SuperSource
+                        weight = endNode.getDegree(Direction.OUTGOING);
+                    }
+
+                    if (endNodeName.equals("T")) {
+                        //SuperSink
+                        weight = startNode.getDegree(Direction.INCOMING);
+                    }
+                }
+
+                output.append("a ").append(startNodeId).append(' ').append(endNodeId).append(" ").append(weight)
+                        //The below line is an extra only to make it easier to read the output
+                        .append(" ( ").append(startNodeName).append(" -> ").append(endNodeName).append(" )")
                         .append(System.lineSeparator());
             }
 
-            Node S = graph.findNode(Label.AXIOM, "name", "S");
-            Node T = graph.findNode(Label.THEOREM, "name", "T");
+            Node S = graph.findNode(Label.AXIOM, "name", SourceNodeName);
+            Node T = graph.findNode(Label.THEOREM, "name", SinkNodeName);
             long S_id = usg.uniqueSequencialId(S.getId());
             long T_id = usg.uniqueSequencialId(T.getId());
 
