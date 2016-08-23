546441bca57f1a95d54c3d892f5a6804d9cf9b6c
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-08-23 10:05:49 -0300

    * Verify nodes of frontier between source and sink sides

--- src/Analysis/Analyze_HIPR_Output.java
+++ src/Analysis/Analyze_HIPR_Output.java
@@ -7,10 +7,18 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Result;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.helpers.collection.MapUtil;
 
 /**
  *
@@ -28,6 +36,7 @@ public class Analyze_HIPR_Output {
         File HIPR_RESULTS
                 = new File("grafo_HIPR_results.txt");
 
+        final File outputFile = new File("grafo_HIPR_sides.txt");
         /**
          * Parse both files
          */
@@ -61,17 +70,52 @@ public class Analyze_HIPR_Output {
         /**
          * Output nodes
          */
-        final File outputFile = new File("grafo_HIPR_sides.txt");
         try (FileWriter fileWriter = new FileWriter(outputFile)) {
             outputFile.createNewFile();
             String lineBreak = System.lineSeparator();
 
             /**
-             * Print nodes on SOURCE side
+             * Print nodes of frontier
              */
             fileWriter
                     .append("#####################")
-                    .append("# NODES ON SOURCE SIDE:")
+                    .append("# NODES OF FRONTIER ")
+                    .append("#####################")
+                    .append(lineBreak).append(lineBreak);
+
+            GraphDatabaseService graph = Graph.GraphFactory.makeGraph("db/super_halved_metamath");
+            try (Transaction tx = graph.beginTx()) {
+                //For each node on the source side
+                for (String sourceNodeName : nodeNamesSourceSide) {
+                    //Search it in the graph to analyse its neighbours
+                    Map<String, Object> param = MapUtil.map("nodeName", sourceNodeName);
+                    Result result = graph.execute("MATCH (n{name: {nodeName}}) RETURN n", param);
+                    //If we find it
+                    if (result.hasNext()) {
+                        Node foundNode = (Node) result.next().get("n");
+                        //Look up all its outgoing relationships
+                        Iterable<Relationship> relationships = foundNode.getRelationships(Direction.OUTGOING);
+                        for (Relationship r : relationships) {
+                            Node neighbour = r.getEndNode();
+                            String neighbourName = neighbour.getProperty("name").toString();
+
+                            //If the name is on the sink side, the source node is a frontier one
+                            if (nodeNamesSinkSide.contains(neighbourName)) {
+                                fileWriter.append(sourceNodeName).append(lineBreak);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            /**
+             * Print nodes on SOURCE side
+             */
+            fileWriter
+                    .append(lineBreak).append(lineBreak)
+                    .append("#####################")
+                    .append("# NODES ON SOURCE SIDE ")
                     .append("#####################")
                     .append(lineBreak).append(lineBreak);
             for (String nodeOnSource : nodeNamesSourceSide.stream().sorted().collect(Collectors.toList())) {
@@ -82,8 +126,9 @@ public class Analyze_HIPR_Output {
              * Print nodes on SINK side
              */
             fileWriter
+                    .append(lineBreak).append(lineBreak)
                     .append("#####################")
-                    .append("# NODES ON SINK SIDE:")
+                    .append("# NODES ON SINK SIDE ")
                     .append("#####################")
                     .append(lineBreak).append(lineBreak);
             for (String nodeOnSink : nodeNamesSinkSide.stream().sorted().collect(Collectors.toList())) {
