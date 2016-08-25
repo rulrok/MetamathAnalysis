3a5ce811524eb441d50e1f3f4ecae6b00a2ff0cf
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-08-25 10:47:03 -0300

    * Organize analysis code

--- src/Analysis/MaxFlowSuperHalved.java
+++ src/Analysis/MaxFlowSuperHalved.java
@@ -7,40 +7,44 @@ import Graph.Algorithms.HalveNodes;
 import Graph.Algorithms.SuperSinkSuperSource;
 import Graph.GraphFactory;
 import Graph.Label;
+import Utils.HIPR;
 import org.neo4j.graphdb.GraphDatabaseService;
 
 /**
  *
  * @author Reuel
  */
-public class MaxFlowHalvedGraphSupersinkSupersource {
+public class MaxFlowSuperHalved {
 
     public static void main(String[] args) {
 
+        System.out.println("Copying original graph...");
         GraphDatabaseService superGraph = GraphFactory.copyGraph("db/metamath", "db/super_halved_metamath");
 
+        System.out.println("Halving nodes...");
         HalveNodes halveNodes = new HalveNodes(superGraph);
         halveNodes
                 .addFilterLabel(Label.AXIOM)
                 .addFilterLabel(Label.THEOREM)
                 .execute();
 
+        System.out.println("Creating super sink and super source...");
         SuperSinkSuperSource sinkSuperSource = new SuperSinkSuperSource(superGraph);
         sinkSuperSource
                 .addFilterLabel(Label.AXIOM)
                 .addFilterLabel(Label.THEOREM)
                 .execute();
 
-        GraphToTxt graphToTxt = new GraphToTxt(superGraph, "grafo_HIPR.txt");
+        System.out.println("Exporting to TXT...");
+        String graphTxtOutput = "grafo_HIPR_super_halved-axiom-theorem.txt";
+        GraphToTxt graphToTxt = new GraphToTxt(superGraph, graphTxtOutput);
         graphToTxt
                 .addFilterLabel(Label.AXIOM)
                 .addFilterLabel(Label.THEOREM)
                 .execute(new HiprFormatter("S", "T", new SuperSinkSourceCustomWeigher()));
 
-//        GraphToHIPRtxt gtHIPR = new GraphToHIPRtxt(superGraph);
-//        gtHIPR
-//                .addFilterLabel(Label.AXIOM)
-//                .addFilterLabel(Label.THEOREM)
-//                .execute("grafo_HIPR_test.txt");
+        System.out.println("Analyzing maxflow with HIPR...");
+        HIPR hipr = new HIPR();
+        hipr.execute(graphTxtOutput, graphTxtOutput.replace(".txt", "_maxflow.txt"));
     }
 }
