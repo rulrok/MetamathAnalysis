f8847757c80d8dd9b756a077da6d12061ad24269
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-09-06 14:22:39 -0300

    * Add script to create theorem only graph

---
+++ src/Analysis/GraphModification/GraphWithTheoremsOnly.java
@@ -0,0 +1,28 @@
+package Analysis.GraphModification;
+
+import Graph.Algorithms.GraphNodeRemover;
+import Graph.GraphFactory;
+import Graph.Label;
+import org.neo4j.graphdb.GraphDatabaseService;
+
+/**
+ *
+ * @author Reuel
+ */
+public class GraphWithTheoremsOnly {
+
+    public static void main(String[] args) {
+        GraphDatabaseService graph = GraphFactory.copyGraph("db/metamath", "db/metamath-theorems-only");
+
+        System.out.println("Removing all nodes but theorems...");
+        GraphNodeRemover gnr = new GraphNodeRemover(graph);
+        gnr.addFilterLabel(Label.AXIOM)
+                .addFilterLabel(Label.CONSTANT)
+                .addFilterLabel(Label.DEFINITION)
+                .addFilterLabel(Label.HYPOTHESIS)
+                .addFilterLabel(Label.SYNTAX_DEFINITION)
+                .addFilterLabel(Label.UNKNOWN)
+                .addFilterLabel(Label.VARIABLE)
+                .execute();
+    }
+}
