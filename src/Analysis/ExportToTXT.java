844658247d86de691e673c7b09d37ac9d5c605d1
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-09-06 17:04:33 -0300

    * Export graph to SIF

--- src/Analysis/ExportToTXT.java
+++ src/Analysis/ExportToTXT.java
@@ -1,10 +1,8 @@
 package Analysis;
 
-import Graph.Algorithms.Export.EdgeWeigher.SuperSinkSourceCustomWeigher;
-import Graph.Algorithms.Export.Formatters.HiprFormatter;
+import Graph.Algorithms.Export.Formatters.*;
 import Graph.Algorithms.Export.GraphToTxt;
 import Graph.GraphFactory;
-import Graph.Label;
 import org.neo4j.graphdb.GraphDatabaseService;
 
 /**
@@ -12,18 +10,16 @@ import org.neo4j.graphdb.GraphDatabaseService;
  * @author Reuel
  */
 public class ExportToTXT {
-
+    
     public static void main(String[] args) {
-        GraphDatabaseService graphDb = GraphFactory.makeDefaultMetamathGraph();
+        GraphDatabaseService graphDb = GraphFactory.makeGraph("db/metamath-theorems-only");
 
         /*
          * Export to txt
          */
         GraphToTxt graphToTxt = new GraphToTxt(graphDb);
         graphToTxt
-                .addFilterLabel(Label.AXIOM)
-                .addFilterLabel(Label.THEOREM)
-                .export("grafo_HIPR_metamath-axiom-theorem.txt", new HiprFormatter("S", "T", new SuperSinkSourceCustomWeigher()));
+                .export("metamath-theorems-only.sif", new SIFFormatter());
     }
-
+    
 }
