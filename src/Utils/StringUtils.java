d3732ea74667335a5cb0cb23ff3165fd589503c9
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-09-19 16:36:48 -0300

    * Create util method for quoting string values

---
+++ src/Utils/StringUtils.java
@@ -0,0 +1,25 @@
+package Utils;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Reuel
+ */
+public class StringUtils {
+
+    public static String quoteValues(String input, String separator) {
+        String[] split = ("" + separator + input).split(separator);
+        String output = Arrays.asList(split).stream().reduce((left, right) -> {
+            return left + "\"" + right.trim() + "\",";
+        }).get();
+        
+        return output.substring(0, output.length() - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String input = "reuel,test, quote_";
+        System.out.println(StringUtils.quoteValues(input, ","));
+    }
+}
