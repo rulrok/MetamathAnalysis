6ba0280e948dde0a722e512d5fa862b2f47ad7d1
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-09-28 13:26:28 -0300

    * Small changes

--- src/Analysis/FlowAnalysis/MaxFlowSuperHalvedTheoremOnly.java
+++ src/Analysis/FlowAnalysis/MaxFlowSuperHalvedTheoremOnly.java
@@ -1,11 +1,9 @@
 package Analysis.FlowAnalysis;
 
-import Graph.Algorithms.Export.Formatters.HiprFormatter;
 import Graph.Algorithms.Export.EdgeWeigher.*;
+import Graph.Algorithms.Export.Formatters.HiprFormatter;
 import Graph.Algorithms.Export.GraphToTxt;
 import Graph.Algorithms.GraphNodeRemover;
-import Graph.Algorithms.HalveNodes;
-import Graph.Algorithms.RemoveIsolatedNodes;
 import Graph.Algorithms.SuperSinkSuperSource;
 import Graph.GraphFactory;
 import Graph.Label;
@@ -15,10 +13,7 @@ import Utils.ParseHIPRInputfile;
 import Utils.ParseHIPROutput;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.List;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
 
 /**
  *
@@ -29,12 +24,9 @@ public class MaxFlowSuperHalvedTheoremOnly {
     public static void main(String[] args) throws FileNotFoundException {
 
         System.out.println("Copying original graph...");
-        GraphDatabaseService graph = GraphFactory.copyGraph(GraphFactory.DEFAULT_METAMATH_DB, "db/metamath_halved_super-theorem-only");
+        GraphDatabaseService graph = GraphFactory.copyGraph(GraphFactory.NOUSERBOX_METAMATH_DB, "db/metamath-nouserboxes_halved_super-theorem-only");
 
         System.out.println("Removing all nodes but theorem ones...");
-        List<String> frontierNodesNames = Arrays.asList(
-                "a1i","a2i","a4i","addass","addcl","adddi","alequcom","alim","cnex","cnre","dfcleq","e0_","gen2","hbn1","id1","idi","iin1","impbox","impxt","merlem1","mp2b","mpg","mulass","mulcl","mulcom","re1ax2","readdcl","remulcl","tbw-ax2","wel","weq","wsb","a17d","alimi","cnrei","hbth","imim2i","merlem2","mp1i","mpd","re1ax2lem","2alimi", "id", "merlem3", "mpi", "syl"
-        );
         GraphNodeRemover gnr = new GraphNodeRemover(graph);
         gnr = gnr
                 .addFilterLabel(Label.AXIOM)
@@ -43,25 +35,8 @@ public class MaxFlowSuperHalvedTheoremOnly {
                 .addFilterLabel(Label.HYPOTHESIS)
                 .addFilterLabel(Label.SYNTAX_DEFINITION)
                 .addFilterLabel(Label.UNKNOWN)
-                .addFilterLabel(Label.VARIABLE)
-                .addCustomFilter((Node node) -> {
-                    return frontierNodesNames.contains(node.getProperty("name").toString());
-                })
-//                .addCustomFilter((Node node) -> {
-//                    return node.getProperty("name").toString().endsWith("OLD");
-//                })
-                ;
+                .addFilterLabel(Label.VARIABLE);
         gnr.execute();
-
-        System.out.println("Removing isolated nodes...");
-        RemoveIsolatedNodes isolatedNodes = new RemoveIsolatedNodes(graph);
-        isolatedNodes.execute();
-
-        System.out.println("Halving nodes...");
-        HalveNodes halveNodes = new HalveNodes(graph);
-        halveNodes
-                .addFilterLabel(Label.THEOREM)
-                .execute();
 
         System.out.println("Creating super sink and super source...");
         SuperSinkSuperSource sinkSuperSource = new SuperSinkSuperSource(graph);
@@ -73,14 +48,14 @@ public class MaxFlowSuperHalvedTheoremOnly {
 
         System.out.println("Exporting to TXT...");
 
-        String graphOutput = "grafo_HIPR_super_halved_inner1_outer2-theorem-only.txt";
+        String graphOutput = "metamath-nouserboxes_super_halved_inner1_outer2-theorem-only.txt";
         String graphFlowOutput = graphOutput.replace(".txt", "_maxflow.txt");
         String graphFlowSidesOutput = graphOutput.replace(".txt", "_sides.txt");
 
-        GraphToTxt graphToTxt = new GraphToTxt(graph);
         HiprFormatter hiprFormatter = new HiprFormatter("S", "T", new InnerOuterEdgeSplittedGraphWeigher(1, 2));
         hiprFormatter = hiprFormatter.setSuperSourceLabel(Label.UNKNOWN).setSuperSinkLabel(Label.UNKNOWN);
 
+        GraphToTxt graphToTxt = new GraphToTxt(graph);
         graphToTxt
                 .addFilterLabel(Label.THEOREM)
                 .addFilterLabel(Label.UNKNOWN)
