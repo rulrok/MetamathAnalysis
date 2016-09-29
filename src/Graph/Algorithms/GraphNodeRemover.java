583d72404479ecb6616de76e17da9988cd832d70
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-09-29 13:54:51 -0300

    * Add the options to delete an entire component based on a start node and its DFS tree onward.

--- src/Graph/Algorithms/GraphNodeRemover.java
+++ src/Graph/Algorithms/GraphNodeRemover.java
@@ -2,13 +2,19 @@ package Graph.Algorithms;
 
 import Graph.Algorithms.Contracts.LabelFiltered;
 import Graph.Label;
+import Graph.RelType;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.logging.Logger;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.traversal.BranchOrderingPolicies;
 import org.neo4j.tooling.GlobalGraphOperations;
 
 /**
@@ -20,7 +26,9 @@ public class GraphNodeRemover implements LabelFiltered {
     List<Label> labelFilters;
     List<Function<Node, Boolean>> customFilters;
 
-    List<Node> nodesToDelete;
+    List<String> componentHeadsNames;
+
+    Set<Node> nodesToDelete;
 
     GraphDatabaseService graph;
 
@@ -28,12 +36,15 @@ public class GraphNodeRemover implements LabelFiltered {
 
         this.graph = graph;
 
+        //DFS components to remove
+        this.componentHeadsNames = new ArrayList<>();
+
         //filter lists
         this.labelFilters = new ArrayList<>();
         this.customFilters = new ArrayList<>();
 
         //found nodes to be deleted at the end
-        this.nodesToDelete = new ArrayList<>();
+        this.nodesToDelete = new HashSet<>();
     }
 
     @Override
@@ -47,8 +58,40 @@ public class GraphNodeRemover implements LabelFiltered {
         return this;
     }
 
+    /**
+     * Starting from a node, delete all the tree found in a DFS search using the
+     * outgoing relationships including the start node. This is executed before
+     * any other removal operation.
+     *
+     * @param startNodeName
+     * @return
+     */
+    public GraphNodeRemover addComponentHeadDFS(String startNodeName) {
+        componentHeadsNames.add(startNodeName);
+        return this;
+    }
+
     public void execute() {
         try (Transaction tx = graph.beginTx()) {
+
+            componentHeadsNames.forEach(nodeName -> {
+                Node foundNode = graph.findNode(Label.AXIOM, "name", nodeName);
+
+                if (foundNode != null) {
+                    graph.traversalDescription()
+                            .depthFirst()
+                            .order(BranchOrderingPolicies.PREORDER_DEPTH_FIRST)
+                            .relationships(RelType.SUPPORTS, Direction.OUTGOING)
+                            .traverse(foundNode)
+                            .nodes()
+                            .forEach(n -> {
+                                Logger.getGlobal().info(
+                                        String.format("\tPreparing to delete the node %s and all its relationships\n", n.getProperty("name").toString())
+                                );
+                                nodesToDelete.add(n);
+                            });
+                }
+            });
 
             GlobalGraphOperations.at(graph).getAllNodes().forEach(node -> {
                 //Try to match with a label filter
