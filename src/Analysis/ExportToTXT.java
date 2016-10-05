f5f12601811637ab9700694b2edc37a01b0ffbd8
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-10-05 09:06:50 -0300

    * Extract filename to constant

--- src/Analysis/ExportToTXT.java
+++ src/Analysis/ExportToTXT.java
@@ -1,6 +1,6 @@
 package Analysis;
 
-import Graph.Algorithms.Export.Formatters.*;
+import Graph.Algorithms.Export.Formatters.SIFFormatter;
 import Graph.Algorithms.Export.GraphToTxt;
 import Graph.GraphFactory;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -10,16 +10,18 @@ import org.neo4j.graphdb.GraphDatabaseService;
  * @author Reuel
  */
 public class ExportToTXT {
-    
+
+    private final static String OUTPUT = "metamath-theorems-only";
+
     public static void main(String[] args) {
-        GraphDatabaseService graphDb = GraphFactory.makeGraph("db/metamath-theorems-only");
+        GraphDatabaseService graphDb = GraphFactory.makeGraph("db/".concat(OUTPUT));
 
         /*
          * Export to txt
          */
         GraphToTxt graphToTxt = new GraphToTxt(graphDb);
         graphToTxt
-                .export("metamath-theorems-only.sif", new SIFFormatter());
+                .export(OUTPUT.concat(".sif"), new SIFFormatter());
     }
-    
+
 }
