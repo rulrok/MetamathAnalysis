ddb87128c283b02a76fb4956f3a6b5ca1770857a
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-10-21 14:23:59 -0200

    * Get the degrees of sources and sinks to evaluate the bottlenecks

--- src/Analysis/FlowAnalysis/IndividualFlows/MaxFlowIndividualSourcesSinksParseResult.java
+++ src/Analysis/FlowAnalysis/IndividualFlows/MaxFlowIndividualSourcesSinksParseResult.java
@@ -1,17 +1,18 @@
 package Analysis.FlowAnalysis.IndividualFlows;
 
-import static Graph.GraphFactory.makeDefaultMetamathGraph;
-import Utils.HistogramUtils;
+import Graph.GraphFactory;
+import Graph.Label;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.function.BiConsumer;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 
 /**
@@ -36,27 +37,63 @@ public class MaxFlowIndividualSourcesSinksParseResult {
         /**
          * Parse each line
          */
+        GraphDatabaseService graph = GraphFactory.makeNoUserboxesMetamathGraph();
+        Map<String, Integer> nodesDegreeCache = new HashMap<>();
+
         Map<String, Double> sourcesFlows = new TreeMap<>();
-        allLines.forEach((String line) -> {
-            String[] values = line.split(" ");
-            String origin = values[0];
-            String destin = values[2];
-            Double maxFlow = Double.parseDouble(values[values.length - 1]);
+        try (Transaction tx = graph.beginTx()) {
+            allLines.forEach((String line) -> {
+                String[] values = line.split(" ");
+                String origin = values[0];
+                String destin = values[2];
+                Double maxFlow = Double.parseDouble(values[values.length - 1]);
 
-            if (sourcesFlows.containsKey(origin)) {
-                Double currentFlow = sourcesFlows.get(origin);
-                currentFlow += maxFlow;
-                sourcesFlows.put(origin, currentFlow);
-            } else {
-                sourcesFlows.put(origin, maxFlow);
-            }
+                if (sourcesFlows.containsKey(origin)) {
+                    Double currentFlow = sourcesFlows.get(origin);
+                    currentFlow += maxFlow;
+                    sourcesFlows.put(origin, currentFlow);
+                } else {
+                    sourcesFlows.put(origin, maxFlow);
+                }
 
-            if (maxFlow > 0) {
-                analysisSB
-                        .append(origin).append(" -> ").append(destin)
-                        .append(" flow: ").append(maxFlow);
-            }
-        });
+                if (maxFlow > 0) {
+
+                    if (!nodesDegreeCache.containsKey(origin)) {
+                        Node nodeFound = graph.findNode(Label.AXIOM, "name", origin.replaceFirst("'", ""));
+                        if (nodeFound == null) {
+                            nodeFound = graph.findNode(Label.THEOREM, "name", origin.replaceFirst("'", ""));
+                        }
+                        int originOutDegree = nodeFound.getDegree(Direction.OUTGOING);
+                        nodesDegreeCache.put(origin, originOutDegree);
+                    }
+                    Integer originOutDegree = nodesDegreeCache.get(origin);
+
+                    if (!nodesDegreeCache.containsKey(destin)) {
+                        Node nodeFound = graph.findNode(Label.THEOREM, "name", origin.replaceFirst("'", ""));
+                        if (nodeFound == null) {
+                            nodeFound = graph.findNode(Label.AXIOM, "name", origin.replaceFirst("'", ""));
+                        }
+                        int destinInDegree = nodeFound.getDegree(Direction.INCOMING);
+                        nodesDegreeCache.put(destin, destinInDegree);
+                    }
+                    Integer destingInDegree = nodesDegreeCache.get(destin);
+
+                    String bottleNeck = "elsewhere";
+                    if (maxFlow.intValue() == originOutDegree) {
+                        bottleNeck = "origin";
+                    } else if (maxFlow.intValue() == destingInDegree) {
+                        bottleNeck = "destin";
+                    }
+
+                    analysisSB
+                            .append(origin).append(" -> ").append(destin)
+                            .append(" flow: ").append(maxFlow)
+                            .append(" origin_out_degree: ").append(originOutDegree)
+                            .append(" desting_in_degree: ").append(destingInDegree)
+                            .append(" bottle_neck: ").append(bottleNeck);
+                }
+            });
+        }
 
         System.out.println("======================================");
         System.out.println("== Max flows:");
