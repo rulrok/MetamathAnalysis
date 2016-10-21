2550d596bec9cce6bb796b52e37d12fc43f74b36
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-10-21 17:42:14 -0200

    * Plot to gnuplot script

--- src/Analysis/FlowAnalysis/IndividualFlows/FlowsFrequencyHistogram.java
+++ src/Analysis/FlowAnalysis/IndividualFlows/FlowsFrequencyHistogram.java
@@ -1,5 +1,8 @@
 package Analysis.FlowAnalysis.IndividualFlows;
 
+import Plot.Gnuplot;
+import Plot.PlotData;
+import Plot.PlotDataSet;
 import Utils.HistogramUtils;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -7,7 +10,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.stream.DoubleStream;
 
 /**
  *
@@ -31,5 +36,20 @@ public class FlowsFrequencyHistogram {
         });
 
         HistogramUtils.PrintHistogram(frequencyHistogram);
+
+        double[] xAxis = frequencyHistogram.entrySet().stream().flatMapToDouble((Entry<Integer, Integer> t) -> DoubleStream.of(t.getKey())).toArray();
+
+        double[] yAxis = frequencyHistogram.entrySet().stream().flatMapToDouble((Entry<Integer, Integer> t) -> DoubleStream.of(t.getValue())).toArray();
+
+        PlotDataSet plotDataSet = new PlotDataSet("Frequency histogram");
+        PlotData plotData = new PlotData("Individual flows", xAxis, yAxis);
+        plotDataSet.addData(plotData);
+        Gnuplot gnuplot = new Gnuplot(plotDataSet);
+        gnuplot
+                .setFilename("frequency_histogram.png")
+                .setxLabel("Flow size from source")
+                .setyLabel("Flow count")
+                .setyLogScale()
+                .plot();
     }
 }
