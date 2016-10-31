ae448e04df389cce3b7dc99cc9feca2039bf9483
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-10-31 17:14:54 -0200

    * Add script to generate file with random values

---
+++ src/Tests/Traversals/randomValuesHistogram.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tests.Traversals;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author reuel
+ */
+public class randomValuesHistogram {
+
+    public static void main(String[] args) {
+        try (FileWriter fw = new FileWriter("rand_values.txt")) {
+            Random r = new Random(2011_1_08_021);
+
+            for (int i = 0; i < 100; i++) {
+                fw.write(Integer.toString(r.nextInt(10)).concat("\n"));
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(randomValuesHistogram.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
