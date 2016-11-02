a14afbb87e867e7f2067a721d0dd80e2a406a113
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-11-02 16:53:07 -0200

    * Don't calculate statistics for the already summarized data

--- src/Analysis/Calculations/Distribution/CalculateDegrees.java
+++ src/Analysis/Calculations/Distribution/CalculateDegrees.java
@@ -24,9 +24,9 @@ public class CalculateDegrees {
         System.out.println("Calculating distribution...");
         DegreeDistribution distribution = new DegreeDistribution(graph);
         distribution.calculate();
-        Map<Integer, Integer> innerDegrees = distribution.getInnerDegrees();
-        Map<Integer, Integer> outerDegrees = distribution.getOuterDegrees();
-        Map<Integer, Integer> allDegrees = distribution.getAllDegrees();
+        Map<Integer, Integer> innerDegreesHistogram = distribution.getInnerDegrees();
+        Map<Integer, Integer> outerDegreesHistogram = distribution.getOuterDegrees();
+        Map<Integer, Integer> allDegreesHistogram = distribution.getAllDegrees();
 
         /*
          * Prepare data
@@ -34,38 +34,17 @@ public class CalculateDegrees {
         System.out.println("Plotting data...");
         PlotDataSet dataSet = new PlotDataSet("Degree distribution");
 
-        double[] innerX = innerDegrees.keySet().stream().mapToDouble(i -> i).toArray();
-        double[] innerY = innerDegrees.values().stream().mapToDouble(i -> i).toArray();
+        double[] innerX = innerDegreesHistogram.keySet().stream().mapToDouble(i -> i).toArray();
+        double[] innerY = innerDegreesHistogram.values().stream().mapToDouble(i -> i).toArray();
         dataSet.addData("Inner degrees", innerX, innerY);
 
-        DescriptiveStatistics innerDS = new DescriptiveStatistics(innerY);
-
-        double[] outerX = outerDegrees.keySet().stream().mapToDouble(i -> i).toArray();
-        double[] outerY = outerDegrees.values().stream().mapToDouble(i -> i).toArray();
+        double[] outerX = outerDegreesHistogram.keySet().stream().mapToDouble(i -> i).toArray();
+        double[] outerY = outerDegreesHistogram.values().stream().mapToDouble(i -> i).toArray();
         dataSet.addData("Outer degrees", outerX, outerY);
 
-        DescriptiveStatistics outerDS = new DescriptiveStatistics(outerY);
-
-        double[] allX = allDegrees.keySet().stream().mapToDouble(i -> i).toArray();
-        double[] allY = allDegrees.values().stream().mapToDouble(i -> i).toArray();
+        double[] allX = allDegreesHistogram.keySet().stream().mapToDouble(i -> i).toArray();
+        double[] allY = allDegreesHistogram.values().stream().mapToDouble(i -> i).toArray();
         dataSet.addData("All degrees", allX, allY);
-
-        DescriptiveStatistics allDS = new DescriptiveStatistics(allY);
-
-        double inner_standardDeviation = innerDS.getStandardDeviation();
-        double inner_mean = innerDS.getMean();
-        System.out.printf("inner : μ = %f; σ = %f\n", inner_mean, inner_standardDeviation);
-        System.out.println(innerDS);
-
-        double outer_standardDeviation = outerDS.getStandardDeviation();
-        double outer_mean = outerDS.getMean();
-        System.out.printf("outer : μ = %f; σ = %f\n", outer_mean, outer_standardDeviation);
-        System.out.println(outerDS);
-
-        double all_standardDeviation = allDS.getStandardDeviation();
-        double all_mean = allDS.getMean();
-        System.out.printf("all   : μ = %f; σ = %f\n", all_mean, all_standardDeviation);
-        System.out.println(allDS);
 
         new Gnuplot(dataSet)
                 .setFilename("grafo_degrees.png")
--- src/Analysis/Calculations/Distribution/ExportDegrees.java
+++ src/Analysis/Calculations/Distribution/ExportDegrees.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
@@ -40,6 +41,25 @@ public class ExportDegrees {
             }
 
         }
+
+        DescriptiveStatistics innerDS = new DescriptiveStatistics(inner.stream().mapToDouble(i -> i).toArray());
+        DescriptiveStatistics outerDS = new DescriptiveStatistics(outer.stream().mapToDouble(i -> i).toArray());
+        DescriptiveStatistics allDS = new DescriptiveStatistics(all.stream().mapToDouble(i -> i).toArray());
+
+        double all_standardDeviation = allDS.getStandardDeviation();
+        double all_mean = allDS.getMean();
+        System.out.printf("all   : μ = %f; σ = %f\n", all_mean, all_standardDeviation);
+        System.out.println(allDS);
+
+        double inner_standardDeviation = innerDS.getStandardDeviation();
+        double inner_mean = innerDS.getMean();
+        System.out.printf("inner : μ = %f; σ = %f\n", inner_mean, inner_standardDeviation);
+        System.out.println(innerDS);
+
+        double outer_standardDeviation = outerDS.getStandardDeviation();
+        double outer_mean = outerDS.getMean();
+        System.out.printf("outer : μ = %f; σ = %f\n", outer_mean, outer_standardDeviation);
+        System.out.println(outerDS);
 
         try (FileWriter fw = new FileWriter("node_degrees.dat")) {
 
