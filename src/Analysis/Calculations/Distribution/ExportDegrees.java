4320fa2f1222d0ecf0bfa91e4143622e23057ef9
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-12-12 09:37:59 -0200

    * Export layer information

--- src/Analysis/Calculations/Distribution/ExportDegrees.java
+++ src/Analysis/Calculations/Distribution/ExportDegrees.java
@@ -1,10 +1,14 @@
 package Analysis.Calculations.Distribution;
 
+import Graph.Algorithms.Contracts.GraphDecomposition;
+import Graph.Algorithms.Decomposition.SimpleGraphDecomposition;
 import Graph.GraphFactory;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
@@ -67,15 +71,44 @@ public class ExportDegrees {
         System.out.printf("outer : μ = %f; σ = %f\n", outer_mean, outer_standardDeviation);
         System.out.println(outerDS);
 
+        ///
+        /// Uncomment this for exporting the layer number
+        ///
+        GraphDecomposition decomposition = new SimpleGraphDecomposition(graph);
+        List<List<Node>> decomposeIntoSources = decomposition.decomposeIntoSources();
+
+        Map<String, Integer> layersNodes = new HashMap<>();
+        try (Transaction tx = graph.beginTx()) {
+            for (int j = 0; j < decomposeIntoSources.size(); j++) {
+                List<Node> layer = decomposeIntoSources.get(j);
+
+                for (Node n : layer) {
+                    String nodeName = n.getProperty("name").toString();
+                    layersNodes.put(nodeName, j + 1);
+                }
+            }
+        }
+       ///
+       /// Uncomment this for exporting the layer number
+       ///
+
         try (FileWriter fw = new FileWriter("node_degrees.csv")) {
 
             String ls = System.lineSeparator();
 
-            fw.write("node;degree;indegree;outdegree" + ls);
+            fw.write("node;degree;indegree;outdegree;layer" + ls);
 
             for (int i = 0; i < degree_array.length; i++) {
 
-                final String string = String.format("%s;%d;%d;%d", nodeNames.get(i), (int) degree_array[i], (int) indegree_array[i], (int) outdegree_array[i]);
+                final String nodeName = nodeNames.get(i);
+
+                final int degree = (int) degree_array[i];
+                final int indegree = (int) indegree_array[i];
+                final int outdegree = (int) outdegree_array[i];
+
+                int layerIndex = layersNodes.getOrDefault(nodeName, -1);
+
+                final String string = String.format("%s;%d;%d;%d;%d", nodeName, degree, indegree, outdegree, layerIndex);
 
                 fw.write(string);
                 fw.write(ls);
