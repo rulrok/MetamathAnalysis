7d6e78dfa75643c870fe2c030f44c6bea87f7410
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-12-16 10:52:02 -0200

    * Collect five pieces of information about one theorem node

--- src/Analysis/SmallerInducedSubgraph/InducedProof706steps.java
+++ src/Analysis/SmallerInducedSubgraph/InducedProof706steps.java
@@ -1,10 +1,24 @@
 package Analysis.SmallerInducedSubgraph;
 
+import Graph.Algorithms.Contracts.GraphDecomposition;
+import Graph.Algorithms.Decomposition.Evaluators.AxiomEvaluator;
+import Graph.Algorithms.Decomposition.SimpleGraphDecomposition;
+import Graph.Algorithms.ReachabilityFromNode;
 import Graph.Algorithms.SmallerInducedSubgraph;
 import Graph.GraphFactory;
 import Graph.Label;
+import Graph.RelType;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
@@ -17,15 +31,49 @@ import org.neo4j.graphdb.Transaction;
 public class InducedProof706steps {
 
     public static void main(String[] args) {
-        GraphDatabaseService graph = GraphFactory.makeNoUserboxesNoJunkAxiomTheoremMetamathGraph();
-        GraphDatabaseService outputGraph = GraphFactory.makeGraph("db/betweenness_706_proof_steps", true);
+        GraphDatabaseService graph = GraphFactory.makeGraph("db/metamath-nouserboxes-axiom-theorem-nojunk");
 
-        List<String> proofElements = new ArrayList<>();
-
+        //Map the values of theorem name and its source decomposition layer number
+        Map<String, Integer> theoremSourceLayer = new HashMap<>(18000);
+        GraphDecomposition sourceDecomposition = new SimpleGraphDecomposition(graph);
+        List<List<Node>> allLayers = sourceDecomposition.decomposeIntoSources();
         try (Transaction tx = graph.beginTx()) {
-            Node t_2503lem2 = graph.findNode(Label.THEOREM, "name", "2503lem2");
+            int layerCount = 0;
+            for (List<Node> layer : allLayers) {
+                layerCount++;
+                for (Node n : layer) {
+                    String name = n.getProperty("name").toString();
+                    theoremSourceLayer.put(name, layerCount);
+                }
+            }
+        }
 
-            t_2503lem2.getRelationships(Direction.INCOMING).forEach(r -> {
+        //Map the number of proof steps of theorem
+        Map<String, Integer> theoremProofSteps = new HashMap<>(18000);
+        try (Scanner scanner = new Scanner(new File("theorems_steps_crawl.txt"))) {
+            String firstLine = scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String nextLine = scanner.nextLine();
+                String[] lineValues = nextLine.split(";");
+                String theoremName = lineValues[1];
+                String theoremSteps = lineValues[2];
+                theoremProofSteps.put(theoremName, Integer.parseInt(theoremSteps));
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(InducedProof706steps.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //Theorem
+        String nodeName = "2503lem2";
+
+        Node currentTheorem;
+
+        //Obtain theorem's proof theorems
+        List<String> proofElements = new ArrayList<>();
+        try (Transaction tx = graph.beginTx()) {
+            currentTheorem = graph.findNode(Label.THEOREM, "name", nodeName);
+
+            currentTheorem.getRelationships(Direction.INCOMING).forEach(r -> {
                 Node proofElement = r.getStartNode();
                 String proofElementName = proofElement.getProperty("name").toString();
 
@@ -33,13 +81,34 @@ public class InducedProof706steps {
             });
         }
 
-        //Prepare algorithm object
+        //Prepare SIS algorithm
+        GraphDatabaseService outputGraph = GraphFactory.makeGraph("db/SIS_2503lem2", true);
         SmallerInducedSubgraph msgi = new SmallerInducedSubgraph(graph, outputGraph);
 
         String[] names = new String[proofElements.size()];
         proofElements.toArray(names);
 
-        //Execute it
         msgi.execute(names);
+
+        //output graph decomposition
+        GraphDecomposition decomposition = new SimpleGraphDecomposition(outputGraph);
+        List<List<Node>> layers = decomposition.decomposeIntoSources();
+        System.out.println("Proof SIS source decomposition: " + layers.size());
+
+        //Reverse reachability
+        ReachabilityFromNode reachabilityFromTheorem = new ReachabilityFromNode(graph);
+        Map<String, Integer> calculate = reachabilityFromTheorem
+                .addEvaluator(new AxiomEvaluator())
+                .reverseGraph()
+                .calculateFromNode(currentTheorem, RelType.SUPPORTS);
+
+        Collection<Integer> reverseRecheability = calculate.values();
+        System.out.println("Reverse recheability: " + reverseRecheability.stream().findFirst().get());
+
+        System.out.println("Theorem proof elements: " + proofElements.size());
+
+        System.out.println("Theorem decomposition layer: " + theoremSourceLayer.get(nodeName));
+
+        System.out.println("Theorem proof steps: " + theoremProofSteps.get(nodeName));
     }
 }
