829a21529c8c9d669106c4ed8b93c082165177b0
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-12-16 09:24:54 -0200

    * Move package

--- src/Analysis/SmallerInducedSubgraph/InducedBetweennessAndStress.java
+++ src/Analysis/SmallerInducedSubgraph/InducedBetweennessAndStress.java
@@ -1,4 +1,4 @@
-package Analysis.Calculations.SmallerInducedSubgraph;
+package Analysis.SmallerInducedSubgraph;
 
 import Graph.Algorithms.SmallerInducedSubgraph;
 import Graph.GraphFactory;
@@ -8,19 +8,20 @@ import org.neo4j.graphdb.GraphDatabaseService;
  *
  * @author Reuel
  */
-public class InducedStress {
+public class InducedBetweennessAndStress {
 
     public static void main(String[] args) {
 
         //Prepare databases
         GraphDatabaseService graph = GraphFactory.makeDefaultMetamathGraph();
-        GraphDatabaseService outputGraph = GraphFactory.makeGraph("db/stress_minimum_induced_graph", true);
+        GraphDatabaseService outputGraph = GraphFactory.makeGraph("db/betweenness_and_stress_minimum_induced_graph", true);
 
         //Prepare algorithm object
         SmallerInducedSubgraph msgi = new SmallerInducedSubgraph(graph, outputGraph);
 
         //Execute it
         msgi.execute(
+                //Stress
                 "peano1",
                 "dvhlvec",
                 "dvhlveclem",
@@ -30,7 +31,18 @@ public class InducedStress {
                 "tfr1a",
                 "frfnom",
                 "ltxrlt",
-                "1nn"
+                "1nn",
+                //Betweenness
+                "fmpt",
+                "dvhlvec",
+                "dvhlveclem",
+                "frfnom",
+                "pwuninel",
+                "occllem",
+                "ltxrlt",
+                "occl",
+                "1nn",
+                "fvex"
         );
     }
 }
